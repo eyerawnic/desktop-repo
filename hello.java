fe1c4129ab6bca9dd24c22d0c5ba9532f7ce73a1
Pratyush <pr6tyush.srivastava@gmail.com>
2024-04-04 15:50:19 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class Hello
 {
     public static void main()
     {
-        System.out.println("Hello");
+        System.out.println("Hello there!!");
+        System.out.println("This is the 2nd line.");
     }
 }
